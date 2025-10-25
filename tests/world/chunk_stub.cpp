66f9e0f9b728b7f09ee86af11eb12bbe235ac63f
cabbabbage <122715492+cabbabbage@users.noreply.github.com>
2025-10-25 06:45:54 -0600

    ok

--- tests/world/chunk_stub.cpp
+++ tests/world/chunk_stub.cpp
@@ -1,5 +1,60 @@
 #include "world/chunk.hpp"
 
-namespace world {
-Chunk::~Chunk() = default;
+#include "core/AssetsManager.hpp"
+#include "dev_mode/dm_styles.hpp"
+#include "dev_mode/font_cache.hpp"
+#include "render/camera.hpp"
+#include "render/global_light_source.hpp"
+#include "render_pipeline/render_asset/shading/ReactiveShadowSettings.hpp"
+
+SDL_Point camera::map_to_screen(SDL_Point world, float, float) const {
+    return world;
+}
+
+SDL_Point camera::screen_to_map(SDL_Point screen, float, float) const {
+    return screen;
+}
+
+Global_Light_Source* Assets::map_light_source() {
+    return nullptr;
+}
+
+const Global_Light_Source* Assets::map_light_source() const {
+    return nullptr;
+}
+
+render_pipeline::shading::ReactiveShadowSettings* Assets::reactive_shadow_settings() {
+    return nullptr;
+}
+
+const render_pipeline::shading::ReactiveShadowSettings* Assets::reactive_shadow_settings() const {
+    return nullptr;
+}
+
+const DMLabelStyle& DMStyles::Label() {
+    static DMLabelStyle style{dm::FONT_PATH, 12, dm::rgba(255, 255, 255, 255)};
+    return style;
+}
+
+DMFontCache& DMFontCache::instance() {
+    static DMFontCache cache;
+    return cache;
+}
+
+DMFontCache::~DMFontCache() = default;
+
+SDL_Point DMFontCache::measure_text(const DMLabelStyle&, const std::string&) const {
+    return SDL_Point{0, 0};
+}
+
+bool DMFontCache::draw_text(SDL_Renderer*,
+                            const DMLabelStyle&,
+                            const std::string&,
+                            int,
+                            int,
+                            SDL_Rect* out_rect) const {
+    if (out_rect) {
+        *out_rect = SDL_Rect{0, 0, 0, 0};
+    }
+    return true;
 }
